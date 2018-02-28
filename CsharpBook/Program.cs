87dae978feb86fc86bcf3da1c051ddf10b65e1ae
Dmitrieva Natalia <idmitrievanm@gmail.com>
2018-03-01 11:26:18 +1300

    change Application Run
    Changed code in Program.cs
    //Application.Run(new Form1());

--- CsharpBook/Program.cs
+++ CsharpBook/Program.cs
@@ -16,7 +16,7 @@ namespace CsharpBook
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormFirstQuize());
+            Application.Run(new Form1());
         }
     }
 }
